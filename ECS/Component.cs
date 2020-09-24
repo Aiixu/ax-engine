8b26511fd19f3928c1137d4530bed5bf37d7e26d
Stévan Corre <stevancorre@gmail.com>
2020-09-24 11:37:58 +0200

    Linked entity methods to components

--- ECS/Component.cs
+++ ECS/Component.cs
@@ -14,10 +14,10 @@ namespace Ax.Engine.ECS
         public virtual void Render(ref OutputHandler.SurfaceItem[,] surface) { }
 
         // Entity implementation
-        public bool HasComponent<T>() where T : Component => Entity.HasComponent<T>();
-        public bool HasComponent<T>(out T c) where T : Component => Entity.HasComponent(out c);
-        public T AddComponent<T>() where T : Component, new() => Entity.AddComponent<T>();
-        public T GetComponent<T>() where T : Component => Entity.GetComponent<T>();
-        public void DestroyComponent<T>() where T : Component => Entity.DestroyComponent<T>();
+        public bool HasComponent<T>() where T : Component => Entity.HasComponent<T>(); 
+        public bool HasComponent<T>(out T c) where T : Component => Entity.HasComponent(out c); 
+        public T AddComponent<T>() where T : Component, new() => Entity.AddComponent<T>(); 
+        public T GetComponent<T>() where T : Component => Entity.GetComponent<T>(); 
+        public void DestroyComponent<T>() where T : Component => Entity.DestroyComponent<T>(); 
     }
 }
